6186c09b72fa15aaadc534e6affc43a5f0b5c2a9
Emil Gaynulin <emil.gaynulin99@bk.ru>
2026-02-19 16:25:20 +0500

    create react project

---
+++ react/src/App.tsx
@@ -0,0 +1,15 @@
+import "../styles/global/_container.scss";
+import "../styles/global/_reset.scss";
+import "../styles/global/_typography.scss";
+import "../styles/global/style.scss";
+import "../styles/global/utilities.scss";
+
+function App() {
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
