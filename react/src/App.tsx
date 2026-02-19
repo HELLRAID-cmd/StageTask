a9d055c5d862407146ef6d0fe2770ea146b87a4f
Emil Gaynulin <emil.gaynulin99@bk.ru>
2026-02-19 16:29:52 +0500

    create form

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -3,11 +3,12 @@ import "../styles/global/_reset.scss";
 import "../styles/global/_typography.scss";
 import "../styles/global/style.scss";
 import "../styles/global/utilities.scss";
+import Form from "./Components/Form/Form";
 
 function App() {
   return (
     <>
-      
+      <Form/>
     </>
   )
 }
