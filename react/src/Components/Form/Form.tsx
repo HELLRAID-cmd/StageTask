8a6e59d5c979249d1819a323b38e1ec828ea8761
Emil Gaynulin <emil.gaynulin99@bk.ru>
2026-02-20 13:37:55 +0500

    I did a URL check

--- react/src/Components/Form/Form.tsx
+++ react/src/Components/Form/Form.tsx
@@ -1,30 +1,56 @@
-import "./Form.scss"
+import { useState } from "react";
+import History from "../History/History";
+import "./Form.scss";
 
 const Form = () => {
+  const [url, setUrl] = useState("");
+  const [err, setErr] = useState("");
+
+  const urlPattern =
+    /^(https?:\/\/)?(([a-z0-9-]+\.)+[a-z]{2,10}|(\d{1,3}\.){3}\d{1,3})(:\d{1,5})?(\/.*)?$/i;
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if (!urlPattern.test(url)) {
+      setErr("Неверный URL");
+      return;
+    }
+
+    setErr("");
+    console.log("Сокращено", url);
+  };
+
   return (
-    <div className="d-flex justify-content-center align-items-center vh-100">
-      <form className="d-flex flex-column w-50 form">
+    <div className="form-wrapper d-flex justify-content-center flex-column align-items-center vh-100">
+      <form className="d-flex flex-column w-50 form" onSubmit={handleSubmit}>
         <div className="mb-3 text-center">
           <label className="form-label fw-medium text-light" htmlFor="url">
             Сокращение URL
           </label>
+          {err && <div className="invalid-feedback d-block">{err}</div>}
           <input
             type="text"
             id="url"
             className="form-control"
-            aria-describedby="URL"
             placeholder="Вставьте URL адрес"
+            value={url}
+            onChange={(e) => setUrl(e.target.value)}
           />
         </div>
         <div className="form-btns btns d-grid gap-4 row-gap-3 justify-content-center">
           <button type="submit" className="btn btn-danger text-light mt-2 col">
             Сократить URL
           </button>
-          <button type="submit" className="btn btn-primary text-light mt-2 col p-3">
+          <button
+            type="button"
+            className="btn btn-primary text-light mt-2 col p-3"
+          >
             Скопировать
           </button>
         </div>
       </form>
+      <History />
     </div>
   );
 };
