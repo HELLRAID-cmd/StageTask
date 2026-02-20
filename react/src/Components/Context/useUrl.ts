8868d765248fb79ae64e9941fb2c1821ecc2a2ef
Emil Gaynulin <emil.gaynulin99@bk.ru>
2026-02-20 14:07:09 +0500

    made context

---
+++ react/src/Components/Context/useUrl.ts
@@ -0,0 +1,8 @@
+import { useContext } from "react";
+import { UrlContext } from "./UrlContext";
+
+export const useUrl = () => {
+  const context = useContext(UrlContext);
+  if (!context) throw new Error("useUrl must be used within a UrlProvider");
+  return context;
+};
