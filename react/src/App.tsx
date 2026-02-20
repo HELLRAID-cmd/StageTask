ffc4a3a21ca83390d73ae620cbdd7a65118f6d77
Emil Gaynulin <emil.gaynulin99@bk.ru>
2026-02-20 14:07:04 +0500

    made context

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,12 +1,13 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "../styles/global/_reset.scss";
 import Form from "./Components/Form/Form";
+import { UrlProvider } from './Components/Context/UrlContext';
 
 function App() {
   return (
-    <>
+    <UrlProvider>
       <Form/>
-    </>
+    </UrlProvider>
   )
 }
 
