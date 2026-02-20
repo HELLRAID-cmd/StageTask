fee740dc8c30e9003677c13b37aa23f530efe30a
Emil Gaynulin <emil.gaynulin99@bk.ru>
2026-02-20 14:33:37 +0500

    added new properties

--- react/src/Components/Context/UrlContext.tsx
+++ react/src/Components/Context/UrlContext.tsx
@@ -1,17 +1,23 @@
-import { createContext, useState, useContext, type ReactNode } from "react";
+import { createContext, useState, type ReactNode } from "react";
 
 export type UrlContextType = {
   url: string;
   setUrl: React.Dispatch<React.SetStateAction<string>>;
+  copy: boolean;
+  setCopy: React.Dispatch<React.SetStateAction<boolean>>;
+  urlHistory: string[];
+  setUrlHistory: React.Dispatch<React.SetStateAction<string[]>>;
 };
 
 export const UrlContext = createContext<UrlContextType | undefined>(undefined);
 
 export const UrlProvider = ({ children }: { children: ReactNode }) => {
   const [url, setUrl] = useState("");
+  const [copy, setCopy] = useState(false);
+  const [urlHistory, setUrlHistory] = useState<string[]>([]);
 
   return (
-    <UrlContext.Provider value={{ url, setUrl }}>
+    <UrlContext.Provider value={{ url, setUrl, copy, setCopy, urlHistory, setUrlHistory }}>
       {children}
     </UrlContext.Provider>
   );
