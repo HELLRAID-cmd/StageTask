e1506abff58b44bc907cab9900829fe203ed64b3
Emil Gaynulin <emil.gaynulin99@bk.ru>
2026-02-21 15:44:38 +0500

    added a delete button

--- react/src/Components/History/History.tsx
+++ react/src/Components/History/History.tsx
@@ -3,20 +3,42 @@ import useCopyToClipboard from "../Utils/useCopyToClipboard";
 import "./History.scss";
 
 const History = () => {
-  const {urlHistory} = useUrl();
+  const { urlHistory, setUrlHistory } = useUrl();
 
   const copyClip = useCopyToClipboard();
-  
+
+  const deleteItem = (id: number) => {
+    setUrlHistory((prev) => prev.filter((_, i) => i !== id));
+  };
+
   return (
-    <div className="history w-50">
+    <div className="history w-50 mb-5">
       <ul className="history-list list-group border-5 rounded-3">
         {urlHistory.map((url, index) => (
-        <li className="history-list__item list-group-item align-items-center bg-transparent d-flex overflow-hidden" key={index}>
-          <a href="#!" className="link-item text-light w-100 d-block p-3" target="_blank">
-            {url}
-          </a>
-          <button className="history-list__btn ms-auto d-block align-content-center h-100 p-3"  onClick={() => copyClip(url)}>Скопировать</button>
-        </li>
+          <li
+            className="history-list__item list-group-item align-items-center bg-transparent d-flex overflow-hidden"
+            key={index}
+          >
+            <a
+              href="#!"
+              className="link-item text-light w-100 d-block p-3"
+              target="_blank"
+            >
+              {url}
+            </a>
+            <button
+              className="history-list__btn ms-auto d-block align-content-center h-100 p-3"
+              onClick={() => copyClip(url)}
+            >
+              Скопировать
+            </button>
+            <button
+              className="history-list__btn ms-auto d-block align-content-center bg-danger h-100 p-3"
+              onClick={() => deleteItem(index)}
+            >
+              Удалить
+            </button>
+          </li>
         ))}
       </ul>
     </div>
