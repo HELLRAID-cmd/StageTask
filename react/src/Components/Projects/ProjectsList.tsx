fd207610fc554af419ed252f1381a7ebaaa00351
Emil Gaynulin <emil.gaynulin99@bk.ru>
2026-02-26 15:44:00 +0500

    Made my own card

--- react/src/Components/Projects/ProjectsList.tsx
+++ react/src/Components/Projects/ProjectsList.tsx
@@ -1,25 +1,61 @@
-import { Card } from "antd";
 import { useProjects } from "../Context/Context";
+import { DeleteOutlined, PlusCircleOutlined } from "@ant-design/icons";
 import { Link } from "react-router-dom";
+import "./Project.scss";
+import "./Card.scss";
 
 const ProjectsList = () => {
   const { projects } = useProjects();
 
   return (
-    <ul className="project-list">
-      {projects.map((project) => (
-        <li className="project-item" key={project.id}>
-          <Link to={`/project/${project.id}`}>
-            <Card
-              title={project.title}
-              style={{ width: 300, background: project.colorCode }}
+    <section className="sect-project">
+      <div className="container">
+        <div className="project">
+          <div className="project-top">
+            <h1 className="project-title">Ваши проекты</h1>
+            <button
+              className="project-top__create"
+              aria-label="Создать проект"
+              type="button"
             >
-              <p>{project.desc}</p>
-            </Card>
-          </Link>
-        </li>
-      ))}
-    </ul>
+              <PlusCircleOutlined style={{ fontSize: "40px" }} />
+            </button>
+          </div>
+          <ul className="project-list">
+            {projects.map((project) => (
+              <li className="project-item" key={project.id}>
+                <Link
+                  className="project-item__link"
+                  to={`/project/${project.id}`}
+                >
+                  <div
+                    className="card"
+                    style={{
+                      background: project.colorCode,
+                    }}
+                  >
+                    <p className="card-title">{project.title}</p>
+
+                    <p className="card-desc">{project.desc}</p>
+                    <div className="card-text">
+                      <p className="card-text__data">
+                        Создано:{" "}
+                        {new Date(project.createdAt).toLocaleDateString(
+                          "ru-RU",
+                        )}
+                      </p>
+                      <button type="button" className="card-title__btn">
+                        <DeleteOutlined style={{ fontSize: "24px" }} />
+                      </button>
+                    </div>
+                  </div>
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </section>
   );
 };
 
