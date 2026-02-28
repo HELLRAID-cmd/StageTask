7f38c1f656288659e00a05417280830de1146c8d
Emil Gaynulin <emil.gaynulin99@bk.ru>
2026-02-28 17:44:15 +0500

    changed the project deletion system

--- react/src/Components/DeleteProject/ModalDelete.tsx
+++ react/src/Components/DeleteProject/ModalDelete.tsx
@@ -1,5 +1,7 @@
-import { Modal } from "antd";
+import { Input, Modal } from "antd";
 import { CloseOutlined } from "@ant-design/icons";
+import { useState } from "react";
+import { useProjects } from "../Context/Context";
 
 type Props = {
   open: boolean;
@@ -8,25 +10,52 @@ type Props = {
 };
 
 const ModalDelete: React.FC<Props> = ({ open, onConfirm, onCancel }) => {
+  const { projects } = useProjects();
+  const [valueDelete, setValueDelete] = useState("");
+
   return (
-    <Modal
-      title="Удалить проект?"
-      closable={{ "aria-label": "Custom Close Button" }}
-      open={open}
-      onOk={onConfirm}
-      onCancel={onCancel}
-      okText="Удалить"
-      okButtonProps={{ danger: true }}
-      cancelButtonProps={{ style: { display: "none" } }}
-      centered
-      closeIcon={
-        <span style={{ color: "#000000", fontSize: "18px" }}>
-          <CloseOutlined />
-        </span>
-      }
-    >
-      <p className=" text-dark">Вы действительно хотите удалить проект?</p>
-    </Modal>
+    <>
+      {projects.map((project) => (
+        <Modal
+          title={`Удалить ${project.title}`}
+          closable={{ "aria-label": "Custom Close Button" }}
+          key={project.id}
+          open={open}
+          onOk={onConfirm}
+          onCancel={onCancel}
+          okText="Удалить"
+          okButtonProps={{
+            style: { width: "100%" },
+            danger: true,
+            disabled: valueDelete.trim() !== project.title,
+          }}
+          cancelButtonProps={{ style: { display: "none" } }}
+          centered
+          style={{ textAlign: "center" }}
+          closeIcon={
+            <span
+              style={{
+                color: "#000000",
+                fontSize: "18px",
+                justifyContent: "center",
+              }}
+            >
+              <CloseOutlined />
+            </span>
+          }
+        >
+          <p className="text-dark mb-2">
+            Чтобы удалить введине название проекта "{project.title}"
+          </p>
+          <Input
+            name="project"
+            placeholder="Введите название"
+            value={valueDelete}
+            onChange={(e) => setValueDelete(e.target.value)}
+          />
+        </Modal>
+      ))}
+    </>
   );
 };
 
