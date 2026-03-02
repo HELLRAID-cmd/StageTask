574a846d0e3b426fe91c0076b622a7642e3cd8c7
Emil Gaynulin <emil.gaynulin99@bk.ru>
2026-03-02 16:11:18 +0500

    Preddal app, under dnd Kit

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -2,30 +2,19 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "../styles/global/_reset.scss";
 import "../styles/global/_antd.scss";
 import "../styles/global/_container.scss";
-import { ProjectProvider, useProjects } from "./Components/Context/Context";
-import Task from "./Components/Task/Task";
-import ProjectsList from "./Components/Projects/ProjectsList";
-import ProjectPage from "./Components/Projects/ProjectPage";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { ProjectProvider } from "./Components/Context/Context";
+import { BrowserRouter as Router } from "react-router-dom";
+import DndContextWrapper from "./Dnd";
 
 function App() {
+
   return (
     <ProjectProvider>
       <Router>
-        <Routes>
-          {/* Главная страница */}
-          <Route path="/" element={<AppContent />} />
-          {/* Страница отдельного проекта */}
-          <Route path="/project/:id" element={<ProjectPage />} />
-        </Routes>
+        <DndContextWrapper/>
       </Router>
     </ProjectProvider>
   );
 }
-
-const AppContent = () => {
-  const { projects } = useProjects();
-  return projects.length > 0 ? <ProjectsList /> : <Task />;
-};
 
 export default App;
