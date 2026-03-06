935123972a650768c8b6d37716c663cd7e19bade
Emil Gaynulin <emil.gaynulin99@bk.ru>
2026-03-06 13:18:46 +0500

    deleted files

--- react/src/Components/Context/useUrl.ts
+++
@@ -1,8 +0,0 @@
-import { useContext } from "react";
-import { Context } from "./Context";
-
-export const useUrl = () => {
-  const context = useContext(Context);
-  if (!context) throw new Error("useUrl must be used within a UrlProvider");
-  return context;
-};
