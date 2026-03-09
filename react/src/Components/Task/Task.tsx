8ed4141855c826b5451076efeeddb3f6e50ebc72
Emil Gaynulin <emil.gaynulin99@bk.ru>
2026-03-09 17:34:33 +0500

    remove style

--- react/src/Components/Task/Task.tsx
+++ react/src/Components/Task/Task.tsx
@@ -4,7 +4,7 @@ import ButtonCreate from "../Projects/ButtonCreate";
 const Task = () => {
 
   return (
-    <div className="task form-wrapper d-flex justify-content-center flex-column align-items-center">
+    <div className="task d-flex justify-content-center flex-column align-items-center">
       <h1 className="task-title fw-medium text-light">У вас нет проекта</h1>
       <h2 className="task-subtitle fw-medium text-light">Создайте его</h2>
       <ButtonCreate/>
