8f9689a27938addd98bcbc7f5acc488dc3d967ae
Emil Gaynulin <emil.gaynulin99@bk.ru>
2026-03-09 16:43:40 +0500

    made pagination

--- react/src/Components/NotFound/NotFound.tsx
+++ react/src/Components/NotFound/NotFound.tsx
@@ -5,12 +5,18 @@ const NotFound = () => {
     <section className="sect-notfound">
       <div className="container">
         <div className="notfound">
-          <img className="notfound-img" src="/notFound/notFound.png" width={300} height={300} alt="Страница не найдена" />
+          <img
+            className="notfound-img"
+            src="/notFound/notFound.png"
+            width={300}
+            height={300}
+            alt="Страница не найдена"
+          />
           <h1 className="notfound-title">Ой такой странице не существует!</h1>
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
 export default NotFound;
