0b97b2e8ef7eca3b64db8f70fee97461eb867a81
Emil Gaynulin <emil.gaynulin99@bk.ru>
2026-03-17 19:12:56 +0500

    Removed double-clicks and made buttons deleting when transferring a task

--- react/src/Components/Task/TaskButtons/TaskButton.tsx
+++ react/src/Components/Task/TaskButtons/TaskButton.tsx
@@ -1,18 +1,16 @@
 import { useDraggable } from "@dnd-kit/core";
 import type { Task } from "../../Utils/type";
 import { useTasks } from "../../Context/ContextTask";
-import TaskChange from "../Tasks/TaskChange";
 import TaskHistoryBtn from "./TaskHistoryBtn";
 import TaskDelete from "./TaskDelete";
+import TaskEdit from "./TaskEdit";
 
 const TaskButton = ({
   task,
   editTaskId,
-  setEditTaskId,
 }: {
   task: Task;
   editTaskId: string | null;
-  setEditTaskId: React.Dispatch<React.SetStateAction<string | null>>;
 }) => {
   const { grabTask } = useTasks();
   const { attributes, listeners, setNodeRef, transform } = useDraggable({
@@ -27,21 +25,12 @@ const TaskButton = ({
       : undefined,
   };
 
-  const handleDoubleClick = (e: React.MouseEvent) => {
-    e.stopPropagation();
-
-    if (task.status === "completed") return;
-
-    setEditTaskId((prev) => (prev === task.id ? null : task.id));
-  };
-
   return (
     <div className="task-item">
       <div
         ref={setNodeRef}
         className="task-item__btns-btn btn text-light w-100 text-start"
         style={{ ...style }}
-        onDoubleClick={handleDoubleClick}
       >
         <span
           {...listeners}
@@ -51,14 +40,19 @@ const TaskButton = ({
           ☰
         </span>
         {editTaskId === task.id ? (
-          <TaskChange input={task.title} taskId={task.id} />
+          <TaskEdit input={task.title} taskId={task.id} />
         ) : (
           <p className="task-item__btns-text">{task.title}</p>
         )}
       </div>
       <div className="task-item__btns-inner">
-        <TaskHistoryBtn taskId={task.id} />
-        {!grabTask && <TaskDelete task={task} />}
+        {!grabTask && (
+          <>
+            <TaskEdit input={task.title} taskId={task.id} />
+            <TaskHistoryBtn taskId={task.id} />
+            <TaskDelete task={task} />
+          </>
+        )}
       </div>
     </div>
   );
