2ad6e4c2b8da4254abece6ed11e39512decf2552
Emil Gaynulin <emil.gaynulin99@bk.ru>
2026-03-20 12:12:35 +0500

    Added a link back, checked the project

--- react/src/Components/Projects/ProjectsList.tsx
+++ react/src/Components/Projects/ProjectsList.tsx
@@ -2,6 +2,8 @@ import "./Project.scss";
 import { useProjects } from "../Context/Context";
 import CardComponent from "../Cards/Card";
 import ButtonCreate from "./ButtonCreate";
+import { LeftCircleOutlined } from "@ant-design/icons";
+import { Link } from "react-router-dom";
 
 const ProjectsList = () => {
   const { projects } = useProjects();
@@ -11,15 +13,30 @@ const ProjectsList = () => {
       <div className="container">
         <div className="project">
           <div className="project-top rounded-3 p-2">
-            <h1 className="project-title">Ваши проекты</h1>
-            <ButtonCreate/>
+            <div className="project-top__left">
+              <Link to={"/"}>
+                <LeftCircleOutlined
+                  style={{ fontSize: "40px", color: "#fff" }}
+                />
+              </Link>
+              <h1 className="project-title">Ваши проекты</h1>
+            </div>
+            <ButtonCreate />
           </div>
           <ul className="project-list">
-            {projects.map((project) => (
-              <li className="project-item" key={project.id}>
-                <CardComponent key={project.id} project={project} />
-              </li>
-            ))}
+            {projects.length === 0 ? (
+              <h1 className="project-list__text text-dark heading-primary">
+                Похоже у вас еще нет проекта, поскорее создайте его!
+              </h1>
+            ) : (
+              <>
+                {projects.map((project) => (
+                  <li className="project-item" key={project.id}>
+                    <CardComponent key={project.id} project={project} />
+                  </li>
+                ))}
+              </>
+            )}
           </ul>
         </div>
       </div>
