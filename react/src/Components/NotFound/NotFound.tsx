187584129ac34afaa69f3887d3d179677b2891ca
Emil Gaynulin <emil.gaynulin99@bk.ru>
2026-03-23 13:42:54 +0500

    changed the page

--- react/src/Components/NotFound/NotFound.tsx
+++ react/src/Components/NotFound/NotFound.tsx
@@ -1,19 +1,32 @@
 import "./NotFound.scss";
 import notFoundImg from "../../assets/notFound/notFound.png";
+import { Link } from "react-router-dom";
 
 const NotFound = () => {
   return (
     <section className="sect-notfound">
       <div className="container">
         <div className="notfound">
-          <img
-            className="notfound-img"
-            src={notFoundImg}
-            width={300}
-            height={300}
-            alt="Страница не найдена"
-          />
-          <h1 className="notfound-title">Ой такой странице не существует!</h1>
+          <div className="notFound-top">
+            <img
+              className="notfound-top__img"
+              src={notFoundImg}
+              width={300}
+              height={300}
+              alt="Страница не найдена"
+            />
+            <h1 className="notfound-top__title">
+              Ой, такой страницы не существует!
+            </h1>
+          </div>
+          <div className="notfound-bottom">
+            <Link className="notfound-bottom__link main" to={"/"}>
+              Вернуться к главной страницы
+            </Link>
+            <Link className="notfound-bottom__link" to={"/myProject"}>
+              Вернуться к проектам
+            </Link>
+          </div>
         </div>
       </div>
     </section>
