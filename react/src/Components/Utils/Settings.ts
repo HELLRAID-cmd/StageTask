e8abf901e25a115c87289e9fd2192a0da46c80e5
Emil Gaynulin <emil.gaynulin99@bk.ru>
2026-04-19 22:59:59 +0500

    добавлены новые настройки

--- react/src/Components/Utils/Settings.ts
+++ react/src/Components/Utils/Settings.ts
@@ -4,3 +4,11 @@ export const MAX_TASK_TEXT = 40;
 // Project
 export const MAX_PROJECT_NAME = 30;
 export const MAX_PROJECT_DESC = 60;
+
+// API
+export const INTERVAL_TIME = 5000;
+export const API_MODE = false;
+export const URL_PROJECT = "http://localhost:3001/project";
+export const HEADERS = {
+  "Content-Type": "application/json",
+};
