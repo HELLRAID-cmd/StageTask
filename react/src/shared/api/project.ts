4d650ef3c0068b9ca18667fdfcb7bd429e95fcda
Emil Gaynulin <emil.gaynulin99@bk.ru>
2026-04-20 16:57:11 +0500

    Made new fetch

--- react/src/shared/api/project.ts
+++ react/src/shared/api/project.ts
@@ -1,8 +1,25 @@
-import { URL_PROJECT } from "../../Components/Utils/Settings";
+import { HEADERS, URL_PROJECT } from "../../Components/Utils/Settings";
+import type { Project } from "../props/type";
 
 const projectsApi = {
   getProject: () => {
     return fetch(URL_PROJECT).then((response) => response.json());
+  },
+
+  add: (project: Omit<Project, "id">) => {
+    return fetch(URL_PROJECT, {
+      method: "POST",
+      headers: HEADERS,
+      body: JSON.stringify(project),
+    }).then((response) => response.json());
+  },
+
+  update: (id: string, data: Partial<Project>) => {
+    return fetch(`${URL_PROJECT}/${id}`, {
+      method: "PATCH",
+      headers: HEADERS,
+      body: JSON.stringify(data),
+    }).then((res) => res.json());
   },
 };
 
