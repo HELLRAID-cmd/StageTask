ec64e3951e56d8516181a4359cac995c5452e487
Emil Gaynulin <emil.gaynulin99@bk.ru>
2026-04-22 00:33:31 +0500

    added new URL

--- react/src/Components/Utils/Settings.ts
+++ react/src/Components/Utils/Settings.ts
@@ -8,7 +8,12 @@ export const MAX_PROJECT_DESC = 60;
 // API
 export const INTERVAL_TIME = 5000;
 export const API_MODE = false;
-export const URL_PROJECT = "http://localhost:3001/project";
 export const HEADERS = {
   "Content-Type": "application/json",
 };
+
+// API PROJECT
+export const URL_PROJECT = "http://localhost:3001/project";
+
+// API TASK
+export const URL_TASK = "http://localhost:3001/task";
