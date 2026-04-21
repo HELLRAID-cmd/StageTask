f6438892dc4a9a623aaf0a8a4aaabc456971f197
Emil Gaynulin <emil.gaynulin99@bk.ru>
2026-04-22 00:31:57 +0500

    changed the creation of a task for the server

--- react/src/Components/Context/Task/useTaskContext.tsx
+++ react/src/Components/Context/Task/useTaskContext.tsx
@@ -1,35 +1,31 @@
-import { useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import type { Task, TaskHistory } from "../../../shared/props/type";
+import tasksApi from "../../../shared/api/task";
+import { API_MODE, INTERVAL_TIME } from "../../Utils/Settings";
+import { useProject } from "../Project/ProjectContext";
 
 const useTaskContext = () => {
-  const [tasks, setTasks] = useState<Task[]>(() => {
-    // загрузка задач из LS
-    const saved = localStorage.getItem("tasks");
-    return saved ? JSON.parse(saved) : [];
-  });
+  const [activeId, setActiveId] = useState<string | null>(null);
+  const [buttonCreate, setButtonCreate] = useState<string[]>([]);
+  const [grabTask, setGrabTask] = useState<boolean>(false);
+  const [editTaskId, setEditTaskId] = useState<string | null>(null);
+  const [errorAPITask, setErrorAPITask] = useState<string | null>(null);
+  const [loadingTask, setLoadingTask] = useState(true);
+
+  const [tasks, setTasks] = useState<Task[]>([]);
+
+  const { activeProjectId } = useProject();
 
   // Функция по созданию задачи
-  const createTask = (title: string, projectId: string, createdAt: number) => {
-    const newTask: Task = {
-      id: crypto.randomUUID(),
-      title,
-      status: "planned",
-      projectId,
-      createdAt,
-      history: [
-        {
-          id: crypto.randomUUID(),
-          type: "created",
-          date: createdAt,
-        },
-      ],
-    };
-    setTasks((prev) => {
-      const updated = [...prev, newTask];
-      localStorage.setItem("tasks", JSON.stringify(updated));
-      return updated;
-    });
-  };
+  const createTask = useCallback(
+    async (data: Omit<Task, "id">) => {
+      if (!activeProjectId) return null;
+
+      const addedTask = await tasksApi.addTask(data);
+      setTasks((prev) => [...prev, addedTask]);
+    },
+    [activeProjectId],
+  );
 
   // функция по изменению текста задачи
   const updateTaskTitle = (id: string, newTitle: string) => {
@@ -57,10 +53,30 @@ const useTaskContext = () => {
     });
   };
 
-  const [activeId, setActiveId] = useState<string | null>(null);
-  const [buttonCreate, setButtonCreate] = useState<string[]>([]);
-  const [grabTask, setGrabTask] = useState<boolean>(false);
-  const [editTaskId, setEditTaskId] = useState<string | null>(null);
+  // Вот так выглядит получение данных через GET
+  useEffect(() => {
+    if (!activeProjectId) return;
+
+    const checkServer = async () => {
+      try {
+        const data = await tasksApi.getTaskByProjectId(activeProjectId);
+        setTasks(data);
+        setErrorAPITask(null);
+      } catch {
+        setErrorAPITask("Ошибка сервера");
+      } finally {
+        setLoadingTask(false);
+      }
+    };
+
+    checkServer();
+
+    if (API_MODE) {
+      const interval = setInterval(checkServer, INTERVAL_TIME);
+
+      return () => clearInterval(interval);
+    }
+  }, [activeProjectId]);
 
   return {
     tasks,
@@ -75,6 +91,10 @@ const useTaskContext = () => {
     editTaskId,
     setEditTaskId,
     updateTaskTitle,
+    errorAPITask,
+    setErrorAPITask,
+    loadingTask,
+    setLoadingTask,
   };
 };
 
