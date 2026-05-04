aaac590d342e99872a7a29f86bb21b11aa4e9627
Emil Gaynulin <emil.gaynulin99@bk.ru>
2026-05-04 23:55:57 +0500

    added icon time

--- react/src/Components/Task/TaskButtons/TaskButton.tsx
+++ react/src/Components/Task/TaskButtons/TaskButton.tsx
@@ -5,6 +5,7 @@ import TaskDelete from "./TaskDelete";
 import TaskEdit from "./TaskEdit";
 import { useTask } from "../../Context/Task/TaskContext";
 import TaskDeadline from "./TaskDeadline";
+import { FieldTimeOutlined } from "@ant-design/icons";
 
 const TaskButton = ({
   task,
@@ -28,6 +29,7 @@ const TaskButton = ({
 
   return (
     <div className="task-item">
+      {task.dueData ? <FieldTimeOutlined style={{ fontSize: "30px", color: "red" }} className="task-item__icon task-item__icon--time"/> : ""}
       <div
         ref={setNodeRef}
         className="task-item__btns-btn btn text-light w-100 text-start"
