7dbae324f9638c055046f1c3e85066a802bba87e
Emil Gaynulin <emil.gaynulin99@bk.ru>
2026-05-21 23:56:56 +0500

    markup column

--- react/src/Components/Task/TaskColumn/TaskColumns.tsx
+++ react/src/Components/Task/TaskColumn/TaskColumns.tsx
@@ -2,26 +2,36 @@ import { Navigate } from "react-router-dom";
 import { useProject } from "../../Context/Project/ProjectContext";
 import TaskColumn from "../Tasks/TaskColumn";
 import ButtonCreateTask from "../../Drag/ButtonCreateTask";
-import type { TaskColumnType } from "../../../shared/props/type";
+import { useColumns } from "../../Context/Columns/ColumnsContext";
 
-function TaskColumns({ id, title }: TaskColumnType) {
-  // const { id } = useParams();
-  const { projects } = useProject();
+function TaskColumns({ projectId }: { projectId: string }) {
+  const { getProject } = useProject();
+  const { columns } = useColumns();
 
-  const project = projects.find((p) => p.id === id);
+  const project = getProject(projectId);
 
   if (!project) {
     return <Navigate to="/notFound" />;
   }
 
   return (
-    <li className="project-item rounded-3 p-2" id={`planned-${project.id}`}>
-      <div className="project-item__top">
-        <p className="project-item__title mb-4">{title}</p>
-      </div>
-      <TaskColumn projectId={project.id} status="planned" />
-      <ButtonCreateTask projectId={project.id} />
-    </li>
+    <>
+      {columns
+        .filter((column) => column.projectId === projectId)
+        .map((column) => (
+          <li
+            className="project-item rounded-3 p-2"
+            id={`planned-${column.id}`}
+            key={column.id}
+          >
+            <div className="project-item__top">
+              <p className="project-item__title mb-4">{column.title}</p>
+            </div>
+            <TaskColumn projectId={column.id} status="planned" />
+            <ButtonCreateTask projectId={column.id} />
+          </li>
+        ))}
+    </>
   );
 }
 
