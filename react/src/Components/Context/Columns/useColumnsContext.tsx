413c2c8d0a7a3dd032a33dbbe74054f391898589
Emil Gaynulin <emil.gaynulin99@bk.ru>
2026-05-21 23:54:32 +0500

    added columns context

---
+++ react/src/Components/Context/Columns/useColumnsContext.tsx
@@ -0,0 +1,64 @@
+import { useCallback, useEffect, useState } from "react";
+import type { Columns } from "../../../shared/props/type";
+import { API_MODE, INTERVAL_TIME } from "../../Utils/Settings";
+import columnsApi from "../../../shared/api/columns";
+
+const useColumnsContext = () => {
+  const [errorAPI, setErrorAPI] = useState<string | null>(null);
+  const [loading, setLoading] = useState(true);
+  const [columns, setColumns] = useState<Columns[]>([]);
+  const [activeColumnsId, setActiveColumnsId] = useState<string | null>(null);
+
+  const getColumns = (id: string) => {
+    return columns.find((t) => t.id === id);
+  };
+
+  // Функция создание колонки
+  const createColumns = useCallback(
+    async (data: Omit<Columns, "id">) => {
+      const addedColumns = await columnsApi.add(data);
+      setColumns((prev) => [...prev, addedColumns]);
+    },
+    [],
+  );
+
+  useEffect(() => {
+    const checkServer = async () => {
+      try {
+        const data = await columnsApi.getColumns();
+        setColumns(data);
+        setErrorAPI(null);
+      } catch {
+        setErrorAPI("Ошибка сервера");
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    checkServer();
+
+    if (API_MODE) {
+      const interval = setInterval(checkServer, INTERVAL_TIME);
+
+      return () => clearInterval(interval);
+    }
+  }, []);
+
+  const isProjectsEmpty = !columns.length;
+
+  return {
+    isProjectsEmpty,
+    getColumns,
+    columns,
+    setColumns,
+    createColumns,
+    activeColumnsId,
+    setActiveColumnsId,
+    loading,
+    setLoading,
+    errorAPI,
+    setErrorAPI,
+  };
+};
+
+export default useColumnsContext;
