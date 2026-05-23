ad7382eec245064ec6513785e1e2b5240784e56e
Emil Gaynulin <emil.gaynulin99@bk.ru>
2026-05-23 15:53:50 +0500

    corrected incorrect arguments

--- react/src/Components/Task/TaskColumn/TaskColumns.tsx
+++ react/src/Components/Task/TaskColumn/TaskColumns.tsx
@@ -1,12 +1,12 @@
 import { Navigate } from "react-router-dom";
 import { useProject } from "../../Context/Project/ProjectContext";
 import TaskColumn from "../Tasks/TaskColumn";
-import ButtonCreateTask from "../../Drag/ButtonCreateTask";
 import { useColumns } from "../../Context/Columns/ColumnsContext";
+import ButtonCreateTask from "./ButtonCreateTask";
 
 function TaskColumns({ projectId }: { projectId: string }) {
   const { getProject } = useProject();
-  const { columns } = useColumns();
+  const { columns, isColumnsEmpty } = useColumns();
 
   const project = getProject(projectId);
 
@@ -16,21 +16,29 @@ function TaskColumns({ projectId }: { projectId: string }) {
 
   return (
     <>
-      {columns
-        .filter((column) => column.projectId === projectId)
-        .map((column) => (
-          <li
-            className="project-item rounded-3 p-2"
-            id={`planned-${column.id}`}
-            key={column.id}
-          >
-            <div className="project-item__top">
-              <p className="project-item__title mb-4">{column.title}</p>
-            </div>
-            <TaskColumn projectId={column.id} status="planned" />
-            <ButtonCreateTask projectId={column.id} />
-          </li>
-        ))}
+      {isColumnsEmpty ? (
+        <h1 className="project-list__text text-light heading-primary">
+          Похоже у вас еще нет колонок, поскорее создайте их!
+        </h1>
+      ) : (
+        <>
+          {columns
+            .filter((column) => column.projectId === projectId)
+            .map((column) => (
+              <li
+                className="project-item rounded-3 p-2"
+                id={`planned-${column.id}`}
+                key={column.id}
+              >
+                <div className="project-item__top">
+                  <p className="project-item__title mb-4">{column.title}</p>
+                </div>
+                <TaskColumn columnId={column.id} projectId={projectId} />
+                <ButtonCreateTask columnId={column.id} projectId={projectId} />
+              </li>
+            ))}
+        </>
+      )}
     </>
   );
 }
